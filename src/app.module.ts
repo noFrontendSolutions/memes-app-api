577b42354be3b2d2bf7b1f844306836aa9ba8792
noFrontendSolutions <b.welzel81@gmail.com>
2022-07-27 14:28:29 +0200

    Before deploying...

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { PublicModule } from './public/public.module';
 import { PublicService } from './public/public.service';
 import { PublicController } from './public/public.controller';
 
-//The ConfigModule is required to get access to the .env file. It's used in prisma.service.ts (using the ConfigService class of the module).
+//The ConfigModule is required to get access to the .env file. It's used in prisma.service.ts (using the ConfigService class of the module)...
 @Module({
   imports: [
     PrismaModule,
